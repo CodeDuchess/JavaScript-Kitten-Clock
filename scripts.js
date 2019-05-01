cdd9565b0455af7552b543adddf5298ff8c79b70
Dana L. Brown <dana.l.brown@gmail.com>
2019-05-01 16:22:47 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,39 @@
+var wakeuptime = 7;
+var noon = 12;
+var lunchtime = 12;
+var naptime = lunchtime + 2;
+var partytime;
+var evening = 18;
+
+// show the current time on the page
+var showCurrentime = function() {
+  // display the string on the webpage
+  var clock = document.getElementById('clock');
+  
+  var CurrentTime = new Date();
+  
+  var hours = currentTime.getHours();
+  var minutes = currentTime.getMinutes();
+  var seconds = currentTime.getSeconds();
+  var meridian = "AM";
+  
+  // set hours
+  if (hours >= noon) {
+      meridian = "PM";
+  }
+  
+  if (hours > noon) {
+     hours = hours - 12;
+  } 
+  
+  //set Minutes
+  
+  if (minutes < 10) {
+      minutes = "0" + seconds;
+  }
+  
+  // put together the string that displays the time
+    var clockTime = hours + ':' + minutes + ':' + seconds + " " + meridian + "!";
+}
+
 
